9cf0d6420d597a47230d2609c3346c4c0d3c69c0
Bartosz Osiński <bartosz.osinski.dev@gmail.com>
2024-02-27 00:19:30 +0100

    client uses baseUrl from env

--- client/src/infrastructure/tvShowRequest.ts
+++ client/src/infrastructure/tvShowRequest.ts
@@ -1,6 +1,7 @@
 import { RatingsDTO } from '../../../shared/RatingsDTO.ts';
+import { baseUrl } from './baseUrl.ts';
 
 export function tvShowRequest(id: string): Promise<RatingsDTO> {
-	return fetch(`/api/ratings?imdbId=${id}`)
+	return fetch(`${baseUrl}/ratings?imdbId=${id}`)
 		.then((response) => response.json());
 }
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+
+	define: {
+		'import.meta.env.API_URL': JSON.stringify(process.env.API_URL)
+	},
 	server: {
 		proxy: {
 			'/api': {
