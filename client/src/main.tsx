b4ecf2cd307b72039bcd25674366f4219ce26c81
Bartosz Osiński <bartosz.osinski.dev@gmail.com>
2024-03-03 19:35:36 +0100

    move global styles to css file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { GlobalStyles } from './util/GlobalStyles.ts';
 import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { TvShowRoute } from './ui/TvShowRoute.tsx';
 import './index.css';
@@ -25,7 +24,6 @@ const router = createHashRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<GlobalStyles/>
 		<RouterProvider router={router} />
 	</React.StrictMode>,
 )
